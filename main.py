d57febd98bc208062d4ed6529a82c4f2397ba0e7
Maira Berlato <mairaberlatof@gmail.com>
2022-07-19 18:56:43 -0300

    matriz de homografia, bounding box e transformar a imagem de saída

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
+import numpy as np
+
 import utils
 
 def main():
   capture = cv2.VideoCapture(0)
   target_image = cv2.imread('resources/book_cover.jpg')
+  output_image = cv2.imread('resources/dog.jpeg')
   
   # detector
   orb_detector = cv2.ORB_create(nfeatures=1000)
@@ -12,7 +15,8 @@ def main():
   image_keypoints, image_descriptor= orb_detector.detectAndCompute(target_image, None)
   
   while True:
-    success, webcam_image = capture.read()
+    # obtendo a imagem da webcam
+    _, webcam_image = capture.read()
     
     # features e descriptors da imagem da webcam
     webcam_keypoints, webcam_descriptor= orb_detector.detectAndCompute(webcam_image, None)
@@ -20,8 +24,19 @@ def main():
     # encontrando os matches entre as duas imagens
     good_matches = utils.get_good_matches(image_descriptor, webcam_descriptor)
     
+    # comparando os keypoints e os bons matches
     image_features = cv2.drawMatches(target_image, image_keypoints, webcam_image, webcam_keypoints, good_matches, None, flags=2)
     
-    utils.display(target_image, webcam_image, image_features)
+    # só prossegue se há mais de 20 bons matches
+    if len(good_matches) > 20:
+      matrix, mask = utils.get_homography(image_keypoints, webcam_keypoints, good_matches)
+      
+      # bounding box - contorno da imagem da webcam
+      bounding_box = utils.get_bounding_box(target_image, webcam_image, matrix)
+      
+      # transformar a imagem de saída
+      warped_image = utils.transform_output(target_image, webcam_image, output_image, matrix)
+    
+      utils.display(target_image, webcam_image, image_features, bounding_box, warped_image)
   
 main()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def get_good_matches(image_descriptor, webcam_descriptor):
   matcher = cv2.BFMatcher()
@@ -11,11 +12,45 @@ def get_good_matches(image_descriptor, webcam_descriptor):
     if m.distance < 0.75 * n.distance:
       good_matches.append(m)
   
-  print(len(good_matches))
   return good_matches
 
-def display(target_image, webcam_image, image_features):
+def get_homography(image_keypoints, webcam_keypoints, good_matches):
+  source_points = np.float32([image_keypoints[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
+  destination_points = np.float32([webcam_keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
+  
+  # matriz de homografia - relaciona as duas imagens
+  matrix, mask = cv2.findHomography(source_points, destination_points, cv2.RANSAC, 5)
+  
+  print(matrix)
+  return matrix, mask
+
+def get_bounding_box(target_image, webcam_image, matrix):
+  image_height, image_width, _ = target_image.shape
+  
+  # pontos baseados no tamanho da imagem alvo
+  points = np.float32([[0,0], [0,image_height], [image_width, image_height], [image_width, 0]]).reshape(-1, 1, 2)
+  
+  # pontos de destino a partir da matriz
+  destinations = cv2.perspectiveTransform(points, matrix)
+  
+  # gerar o contorno do bounding box
+  return cv2.polylines(webcam_image, [np.int32(destinations)], True, (255,0,255), 3)
+
+def transform_output(target_image, webcam_image, output_image, matrix):
+  # tamanhos
+  target_height, target_width, _ = target_image.shape
+  webcam_height, webcam_width, _ = webcam_image.shape
+  
+  # assegurar que as imagens têm o mesmo tamanho
+  output_image = cv2.resize(output_image, (target_width, target_height))
+  
+  # transformar a imagem de saída
+  return cv2.warpPerspective(output_image, matrix, (webcam_width, webcam_height))
+
+def display(target_image, webcam_image, image_features, bounding_box, output_image):
   cv2.imshow("features", image_features)
-  cv2.imshow("target", target_image)
-  cv2.imshow("webcam", webcam_image)
-  cv2.waitKey(1)
+  #cv2.imshow("target", target_image)
+  #cv2.imshow("webcam", webcam_image)
+  cv2.imshow("bounding box", bounding_box)
+  cv2.imshow("output", output_image)
+  cv2.waitKey(0)
